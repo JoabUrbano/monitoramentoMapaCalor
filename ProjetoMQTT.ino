0cbf123095d539381646d7850a01c92fcd0821e8
Joab Urbano <joab.uaraujo@gmail.com>
2023-10-17 19:08:27 -0300

    Implementação da publicação dos dados coletados pelos sensores no Adafruit IO

--- ProjetoMQTT.ino
+++ ProjetoMQTT.ino
@@ -4,10 +4,12 @@
 #include <WiFiClient.h>
 #include <PubSubClient.h>
 
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+
 /***** Definições de variaveis *****/
 
 #define DHT_PIN01 15
-
 #define DHT_PIN02 18
 
 #define DHTTYPE DHT11
@@ -21,54 +23,32 @@ DHT dht02(DHT_PIN02, DHTTYPE);
 WiFiClient wifi_client;
 PubSubClient mqtt_client(wifi_client);
 
-const char* wifi_ssid = "imd0902";
-const char* wifi_password = "imd0902iot";
+#define wifi_ssid "imd0902"
+#define wifi_password "imd0902iot"
 int wifi_timeout = 100000;
 
-const char* mqtt_broker = "broker.hivemq.com";
+#define mqtt_broker "io.adafruit.com"
 const int mqtt_port = 1883;
 int mqtt_timeout = 10000;
 
+#define mqtt_usernameAdafruitIO "Joab_Urbano"
+#define mqtt_keyAdafruitIO "aio_mVdl92oNiSTLQxiTKRH3ixlxcOgu"
 
+/***** Configuração dos feeds *****/
+Adafruit_MQTT_Client mqtt(&wifi_client, mqtt_broker, mqtt_port, mqtt_usernameAdafruitIO, mqtt_usernameAdafruitIO, mqtt_keyAdafruitIO);
+Adafruit_MQTT_Publish estadoSensores = Adafruit_MQTT_Publish(&mqtt, mqtt_usernameAdafruitIO "/feeds/estadoSensores");
+Adafruit_MQTT_Publish estadoSensoresHumidade = Adafruit_MQTT_Publish(&mqtt, mqtt_usernameAdafruitIO "/feeds/estadoSensoresHumidade");
+Adafruit_MQTT_Publish temperaturaMediaTopico = Adafruit_MQTT_Publish(&mqtt, mqtt_usernameAdafruitIO "/feeds/temperaturaMedia");
+Adafruit_MQTT_Publish humidadeMediaTopico = Adafruit_MQTT_Publish(&mqtt, mqtt_usernameAdafruitIO "/feeds/humidadeMedia");
+Adafruit_MQTT_Publish temperaturaTopico01 = Adafruit_MQTT_Publish(&mqtt, mqtt_usernameAdafruitIO "/feeds/temperatura01");
+Adafruit_MQTT_Publish temperaturaTopico02 = Adafruit_MQTT_Publish(&mqtt, mqtt_usernameAdafruitIO "/feeds/temperatura02");
+Adafruit_MQTT_Publish humidadeTopico01 = Adafruit_MQTT_Publish(&mqtt, mqtt_usernameAdafruitIO "/feeds/humidade01");
+Adafruit_MQTT_Publish humidadeTopico02 = Adafruit_MQTT_Publish(&mqtt, mqtt_usernameAdafruitIO "/feeds/humidade02");
+
+/***** Funções utilizadas *****/
 void imprimeSerial(float temperatura01, float temperatura02, float humidade01, float humidade02, float variacaoTemperaturaSensores, float variacaoHumidadeSensores, float temperaturaMedia, float humidadeMedia, float hic01, float hic02, bool temperaturaDiscrepante, bool humidadeDiscrepante);
-
-void connectWiFi(){
-  Serial.print("Conectando à rede WiFi .. ");
-
-  unsigned long tempoInicial = millis();
-  while(WiFi.status() != WL_CONNECTED && (millis() - tempoInicial < wifi_timeout)){
-    Serial.print(".");
-    delay(100);
-  }
-  Serial.println();
-
-  if(WiFi.status() != WL_CONNECTED){
-    Serial.println("Conexão com WiFi falhou!");
-  } else{
-    Serial.print("Conectado com o IP: ");
-    Serial.println(WiFi.localIP());
-  }
-}
-
-void connectMQTT() {
-  unsigned long tempoInicial = millis();
-  while (!mqtt_client.connected() && (millis() - tempoInicial < mqtt_timeout)) {
-      if (WiFi.status() != WL_CONNECTED){
-        connectWiFi();
-      }
-      Serial.print("Conectando ao MQTT Broker..");
-      
-      if (mqtt_client.connect("ESP32Client")) {
-        Serial.println();
-        Serial.print("Conectado ao broker MQTT!");
-      } else{
-        Serial.println();
-        Serial.print("Conexão com o broker MQTT falhou!");
-      }
-      delay(100);
-  }
-  Serial.println();
-}
+void connectWiFi();
+void connectMQTT();
 
 void setup()
 {
@@ -89,8 +69,8 @@ void setup()
 void loop()
 {
   // Verificando conexão
-  if (!mqtt_client.connected()){
-    connectMQTT();
+  if (!mqtt_client.connected()){ // Se MQTT não estiver conectado
+    connectMQTT(); // Tente se conectar ao broker
   }
 
   if (mqtt_client.connected()){
@@ -100,13 +80,13 @@ void loop()
 
   float humidade01 = dht01.readHumidity();
 
-  float humidade02 = dht02.readHumidity();
+  float humidade02 = dht02.readHumidity(); //31.0
 
   // Leitura da temperatura em Celcius
 
   float temperatura01 = dht01.readTemperature();
 
-  float temperatura02 = dht02.readTemperature();
+  float temperatura02 = dht02.readTemperature(); //27.0
 
   // Calculo da media das netricas e a variação entre sensires
   float variacaoTemperaturaSensores = abs(temperatura01 - temperatura02);
@@ -134,23 +114,77 @@ void loop()
   }
 
   // Capta a sensação termica
-
   float hic01 = dht01.computeHeatIndex(temperatura01, humidade01, false);
   float hic02 = dht01.computeHeatIndex(temperatura02, humidade02, false);
 
   // Checa se temperatura ou humidade estão discrepantes
-
   bool temperaturaDiscrepante = variacaoTemperaturaSensores > 5;
   bool humidadeDiscrepante = variacaoHumidadeSensores > 10;
 
   imprimeSerial(temperatura01, temperatura02, humidade01, humidade02, variacaoTemperaturaSensores, variacaoHumidadeSensores, temperaturaMedia, humidadeMedia, hic01, hic02, temperaturaDiscrepante, humidadeDiscrepante);
 
-  // Publicações nos topicos
-  mqtt_client.publish("/imd0902/projeto1/mediaTemperaturas", String(temperaturaMedia).c_str(), true);
-  mqtt_client.publish("/imd0902/projeto1/mediaHumidades", String(humidadeMedia).c_str(), true);
+  // Publica uma mensagem indicando se a temperatura é discrepante ou não
+  if(temperaturaDiscrepante) 
+    estadoSensores.publish("Temperatura discrepante!");
+  else
+    estadoSensores.publish("Valores de temperatura semelhantes!");
+  
+  // Publica uma mensagem indicando se a humidade é discrepante ou não
+  if(humidadeDiscrepante) 
+    estadoSensoresHumidade.publish("Humidade discrepante!");
+  else
+    estadoSensoresHumidade.publish("Valores de humidade semelhantes!");
+  
+  // Faz a publicação dos valores de temperatura e humidade dos sensores e suas médias
+  temperaturaMediaTopico.publish(temperaturaMedia);
+  temperaturaTopico01.publish(temperatura01);
+  temperaturaTopico02.publish(temperatura02);
+  humidadeMediaTopico.publish(humidadeMedia);
+  humidadeTopico01.publish(humidade01);
+  humidadeTopico02.publish(humidade02);
 
-  delay(5000);
+  delay(20000);
   }
+}
+
+// Função para conectar o Esp ao Wifi
+void connectWiFi() {
+  Serial.print("Conectando à rede WiFi .. ");
+
+  unsigned long tempoInicial = millis();
+  while (WiFi.status() != WL_CONNECTED && (millis() - tempoInicial < wifi_timeout)) {
+    Serial.print(".");
+    delay(100);
+  }
+  Serial.println();
+
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Conexão com WiFi falhou!");
+  } else {
+    Serial.print("Conectado com o IP: ");
+    Serial.println(WiFi.localIP());
+  }
+}
+
+// Função para conexão no broker MQTT
+void connectMQTT() {
+  unsigned long tempoInicial = millis();
+  while (!mqtt_client.connected() && (millis() - tempoInicial < mqtt_timeout)) {
+    if (WiFi.status() != WL_CONNECTED) {
+      connectWiFi();
+    }
+    Serial.print("Conectando ao MQTT Broker..");
+
+    if (mqtt_client.connect("ESP32Client", mqtt_usernameAdafruitIO, mqtt_keyAdafruitIO)) {
+      Serial.println();
+      Serial.print("Conectado ao broker MQTT!");
+    } else {
+      Serial.println();
+      Serial.print("Conexão com o broker MQTT falhou!");
+      delay(500);
+    }
+  }
+  Serial.println();
 }
 
 // Função para imprimir na serial os dados coletados e suas medias
--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 # projetoIntroducaoIot01
 Código do primeiro projeto da materia de introdução a internet das coisas. Esse projeto consiste em um controlador de temperatura com mais de um sensor DHT11 para garantir a redundância dos dados e possibilitar a criação de uma aproximação de mapa de calor.
 
+Componentes utilizados:
+- Esp32 dev module
+- Protoboard
+- Jumpers
+- Dois sensores DHT11
+- Dois resistores de 1kOhm
+
 Bibliotecas necessarioas:
 - Dht Sensor Library
 - Adafruit Unified Sensor
 - Esp32
 - Wifi
+- Adafruit MQTT Library
