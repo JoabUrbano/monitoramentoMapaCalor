0b5e2d4f9249bac97051412c633b38d8773f77a7
Gabriel Dantas <gabrielnsdantas@gmail.com>
2023-10-26 09:44:29 -0300

    Adicionado led de indicacao de conexao wifi

--- ProjetoMQTT.ino
+++ ProjetoMQTT.ino
@@ -11,6 +11,7 @@
 
 #define DHT_PIN01 15
 #define DHT_PIN02 18
+#define LED_PIN 2
 
 #define DHTTYPE DHT11
 
@@ -54,6 +55,8 @@ void setup()
 {
   Serial.begin(9600);
 
+  pinMode(LED_PIN, OUTPUT);
+
   /***** Variaveis para conexão MQTT *****/
 
   WiFi.mode(WIFI_STA); //"station mode": permite o ESP32 ser um cliente da rede WiFi
@@ -69,116 +72,131 @@ void setup()
 void loop()
 {
   // Verificando conexão
-  if (!mqtt_client.connected()){ // Se MQTT não estiver conectado
+  if (!mqtt_client.connected())
+  {                // Se MQTT não estiver conectado
     connectMQTT(); // Tente se conectar ao broker
   }
 
-  if (mqtt_client.connected()){
+  if (mqtt_client.connected())
+  {
     mqtt_client.loop();
 
-  // Leitura da humidade dos sensores
+    // Leitura da humidade dos sensores
 
-  float humidade01 = dht01.readHumidity();
+    float humidade01 = dht01.readHumidity();
 
-  float humidade02 = dht02.readHumidity(); //31.0
+    float humidade02 = dht02.readHumidity(); // 31.0
 
-  // Leitura da temperatura em Celcius
+    // Leitura da temperatura em Celcius
 
-  float temperatura01 = dht01.readTemperature();
+    float temperatura01 = dht01.readTemperature();
 
-  float temperatura02 = dht02.readTemperature(); //27.0
+    float temperatura02 = dht02.readTemperature(); // 27.0
 
-  // Calculo da media das netricas e a variação entre sensires
-  float variacaoTemperaturaSensores = abs(temperatura01 - temperatura02);
+    // Calculo da media das netricas e a variação entre sensires
+    float variacaoTemperaturaSensores = abs(temperatura01 - temperatura02);
 
-  float variacaoHumidadeSensores = abs(humidade01 - humidade02);
+    float variacaoHumidadeSensores = abs(humidade01 - humidade02);
 
-  float temperaturaMedia = (temperatura01 + temperatura02) / 2;
+    float temperaturaMedia = (temperatura01 + temperatura02) / 2;
 
-  float humidadeMedia = (humidade01 + humidade02) / 2;
+    float humidadeMedia = (humidade01 + humidade02) / 2;
 
-  // Checa se alguma das leituras do sensor 01 falhou e tenta novamente
+    // Checa se alguma das leituras do sensor 01 falhou e tenta novamente
 
-  if (isnan(humidade01) || isnan(temperatura01))
-  {
-    Serial.println(F("Falha ao ler as informações do sensor 01!"));
-    return;
-  }
+    if (isnan(humidade01) || isnan(temperatura01))
+    {
+      Serial.println(F("Falha ao ler as informações do sensor 01!"));
+      return;
+    }
 
-  // Checa se alguma das leituras do sensor 02 falhou e tenta novamente
+    // Checa se alguma das leituras do sensor 02 falhou e tenta novamente
 
-  if (isnan(humidade02) || isnan(temperatura02))
-  {
-    Serial.println(F("Falha ao ler as informações do sensor 02!"));
-    return;
-  }
+    if (isnan(humidade02) || isnan(temperatura02))
+    {
+      Serial.println(F("Falha ao ler as informações do sensor 02!"));
+      return;
+    }
 
-  // Capta a sensação termica
-  float hic01 = dht01.computeHeatIndex(temperatura01, humidade01, false);
-  float hic02 = dht01.computeHeatIndex(temperatura02, humidade02, false);
+    // Capta a sensação termica
+    float hic01 = dht01.computeHeatIndex(temperatura01, humidade01, false);
+    float hic02 = dht01.computeHeatIndex(temperatura02, humidade02, false);
 
-  // Checa se temperatura ou humidade estão discrepantes
-  bool temperaturaDiscrepante = variacaoTemperaturaSensores > 5;
-  bool humidadeDiscrepante = variacaoHumidadeSensores > 10;
+    // Checa se temperatura ou humidade estão discrepantes
+    bool temperaturaDiscrepante = variacaoTemperaturaSensores > 5;
+    bool humidadeDiscrepante = variacaoHumidadeSensores > 10;
 
-  imprimeSerial(temperatura01, temperatura02, humidade01, humidade02, variacaoTemperaturaSensores, variacaoHumidadeSensores, temperaturaMedia, humidadeMedia, hic01, hic02, temperaturaDiscrepante, humidadeDiscrepante);
+    imprimeSerial(temperatura01, temperatura02, humidade01, humidade02, variacaoTemperaturaSensores, variacaoHumidadeSensores, temperaturaMedia, humidadeMedia, hic01, hic02, temperaturaDiscrepante, humidadeDiscrepante);
 
-  // Publica uma mensagem indicando se a temperatura é discrepante ou não
-  if(temperaturaDiscrepante) 
-    estadoSensores.publish("Temperatura discrepante!");
-  else
-    estadoSensores.publish("Valores de temperatura semelhantes!");
-  
-  // Publica uma mensagem indicando se a humidade é discrepante ou não
-  if(humidadeDiscrepante) 
-    estadoSensoresHumidade.publish("Humidade discrepante!");
-  else
-    estadoSensoresHumidade.publish("Valores de humidade semelhantes!");
-  
-  // Faz a publicação dos valores de temperatura e humidade dos sensores e suas médias
-  temperaturaMediaTopico.publish(temperaturaMedia);
-  temperaturaTopico01.publish(temperatura01);
-  temperaturaTopico02.publish(temperatura02);
-  humidadeMediaTopico.publish(humidadeMedia);
-  humidadeTopico01.publish(humidade01);
-  humidadeTopico02.publish(humidade02);
+    // Publica uma mensagem indicando se a temperatura é discrepante ou não
+    if (temperaturaDiscrepante)
+      estadoSensores.publish("Temperatura discrepante!");
+    else
+      estadoSensores.publish("Valores de temperatura semelhantes!");
 
-  delay(20000);
+    // Publica uma mensagem indicando se a humidade é discrepante ou não
+    if (humidadeDiscrepante)
+      estadoSensoresHumidade.publish("Humidade discrepante!");
+    else
+      estadoSensoresHumidade.publish("Valores de humidade semelhantes!");
+
+    // Faz a publicação dos valores de temperatura e humidade dos sensores e suas médias
+    temperaturaMediaTopico.publish(temperaturaMedia);
+    temperaturaTopico01.publish(temperatura01);
+    temperaturaTopico02.publish(temperatura02);
+    humidadeMediaTopico.publish(humidadeMedia);
+    humidadeTopico01.publish(humidade01);
+    humidadeTopico02.publish(humidade02);
+
+    delay(20000);
   }
 }
 
 // Função para conectar o Esp ao Wifi
-void connectWiFi() {
+void connectWiFi()
+{
+  digitalWrite(LED_PIN, LOW);
   Serial.print("Conectando à rede WiFi .. ");
 
   unsigned long tempoInicial = millis();
-  while (WiFi.status() != WL_CONNECTED && (millis() - tempoInicial < wifi_timeout)) {
+  while (WiFi.status() != WL_CONNECTED && (millis() - tempoInicial < wifi_timeout))
+  {
     Serial.print(".");
     delay(100);
   }
   Serial.println();
 
-  if (WiFi.status() != WL_CONNECTED) {
+  if (WiFi.status() != WL_CONNECTED)
+  {
     Serial.println("Conexão com WiFi falhou!");
-  } else {
+  }
+  else
+  {
+    digitalWrite(LED_PIN, HIGH);
     Serial.print("Conectado com o IP: ");
     Serial.println(WiFi.localIP());
   }
 }
 
 // Função para conexão no broker MQTT
-void connectMQTT() {
+void connectMQTT()
+{
   unsigned long tempoInicial = millis();
-  while (!mqtt_client.connected() && (millis() - tempoInicial < mqtt_timeout)) {
-    if (WiFi.status() != WL_CONNECTED) {
+  while (!mqtt_client.connected() && (millis() - tempoInicial < mqtt_timeout))
+  {
+    if (WiFi.status() != WL_CONNECTED)
+    {
       connectWiFi();
     }
     Serial.print("Conectando ao MQTT Broker..");
 
-    if (mqtt_client.connect("ESP32Client", mqtt_usernameAdafruitIO, mqtt_keyAdafruitIO)) {
+    if (mqtt_client.connect("ESP32Client", mqtt_usernameAdafruitIO, mqtt_keyAdafruitIO))
+    {
       Serial.println();
       Serial.print("Conectado ao broker MQTT!");
-    } else {
+    }
+    else
+    {
       Serial.println();
       Serial.print("Conexão com o broker MQTT falhou!");
       delay(500);
